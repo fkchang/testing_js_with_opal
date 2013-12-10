6b767a594ca9c0fd72b22d147a255c3dfc80f6e8
Forrest Chang <fchang@hedgeye.com>
2013-12-10 12:28:00 -0800

    add more comments

--- spec/js-class-to-be-tested_spec.rb
+++ spec/js-class-to-be-tested_spec.rb
@@ -21,30 +21,45 @@ end
 
 describe "Dom Testing" do
   it "should see DOM changes" do
-
+    # convenient to use heredocs to define HTML inline
+    # in a very readable, close to test code context
     html = <<-HTML
       <div id="target-div">
         original text
       </div>
     HTML
 
+    # if using opal-jquery, you can use Element.parse to create
+    # DOM elements and append to the body of the DOM used the
+    # registered index.html
     div = Element.parse html
     div.append_to_body
 
+    # gran an element via opal-jquery
     d = Element.find("#target-div")
     d.text.should =~ /original text/
+
+    # exercise your js code on the DOM object
     `document.getElementById('target-div').innerText = 'js replaced text'`
 
+    # do your assertions
     d.text.should == "js replaced text"
   end
 
 end
 
 describe "Async testing" do
-  async "should test async" do
-    puts "starting async"
+
+  async "can test async javascript" do
+
+    puts "starting async"       # proof to myself this works
     `var foo = 1`
+    # you can assert values on js
     `foo`.should == 1
+
+    # you can call js via xstrings
+    # opal is smart enough to expand the run_async line via string
+    #interpolation.
     %x| setTimeout( function()
                      { console.log('in timeout');
                        foo = 2;
